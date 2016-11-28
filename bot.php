4827560e459583570b4cf144c56c64c315198a6f
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-11-28 21:59:05 +0700

    update

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
       settype($text, "integer");
 
       if ( !(($text > 0) and ($text <= 999999) and $len == 6)){
-        $replyMsg = "เดี๋ยวปั๊ดเหนี่ยวเลย ใส่ตัวเลข 6 หลักเท่านั้นนะจ๊ะ ".$text;
+        $replyMsg = "เดี๋ยวปั๊ดเหนี่ยวเลย ใส่ตัวเลข 6 หลักเท่านั้นนะจ๊ะ";
       }else{
 
       // Get Lotto Date
