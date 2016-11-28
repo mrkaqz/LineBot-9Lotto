587ae6fa52878812701a84852a0174587e1e9582
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-11-29 01:06:29 +0700

    debug

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ $year = rand(2015,2018);
 $month = rand(01,12);
 $day = rand(01,31);
 if ($day >= 16) {
-  $lottoday = 16;
+  $lottoday = "16";
 }else{
-  $lottoday = 1;
+  $lottoday = "01";
 }
 $lottodate = $year."-".$month."-".$lottoday;
 echo $year."-".$month."-".$day;
