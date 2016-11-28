ac83658a5b8635ef5f506b92c0fbb92538598e71
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-11-29 01:48:37 +0700

    debug

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 echo "Hello LINE BOT <br />";
 
+$text = "ตรวจหวย​ 326524";
 // Lotto
 
 $number = iconv_substr($text,8);
@@ -143,30 +144,6 @@ else {
 }
 
 // Build message to reply back
-
-$messages = [
-  'type' => 'text',
-  'text' => $replyMsg
-];
-
-// Make a POST Request to Messaging API to reply to sender
-$url = 'https://api.line.me/v2/bot/message/reply';
-$data = [
-  'replyToken' => $replyToken,
-  'messages' => [$messages],
-];
-$post = json_encode($data);
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-
-echo $result . "\r\n";
+echo $replyMsg;
 
 }
