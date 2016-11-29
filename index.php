8ff8e3e3aeb80d571a83a2cfb4f4713f2947da44
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-11-30 00:17:32 +0700

    update 1

--- index.php
+++ index.php
@@ -6,6 +6,5 @@ $content = file_get_contents('https://api.rss2json.com/v1/api.json?rss_url=http%
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if ($events['status'] == "OK") {
+
   echo $content;
-}
