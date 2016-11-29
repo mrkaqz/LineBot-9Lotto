e90107942acfb5702a4ec95de58392ad42dbeaf3
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-11-29 17:45:45 +0700

    join msg

--- bot.php
+++ bot.php
@@ -208,7 +208,43 @@ if (!is_null($events['events'])) {
 
 		}
 
-		}
+	}elseif ($event['type'] == 'join') {
+
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+
+			$messages = [
+				'type' => 'text',
+				'text' => 'วิธีตรวจหวยงวดล่าสุด\n ให้พิมพ์คำว่า ตรวจหวย เว้นวรรค ตามด้วยตัวเลข 6 หลัก เช่น \n "ตรวจหวย 123456"'
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+
+
+
+	}
+
+
+
 	}
 }
 echo "OK";
