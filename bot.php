39710bd5015c2d750c15b372f67dbce4399d1636
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-11-30 01:15:17 +0700

    เรียงเบอร์

--- bot.php
+++ bot.php
@@ -225,6 +225,34 @@ if (!is_null($events['events'])) {
 				$replyMsg .= "ผลรางวัล:".chr(10);
 				$replyMsg .= str_replace('<br>',chr(10),$levents['items'][$i]['content']);
 			}
+
+			// Build message to reply back
+
+			$messages = [
+				'type' => 'text',
+				'text' => $replyMsg
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+
 		}
 
 	}elseif ($event['type'] == 'join') {
