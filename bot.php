325d350585904ee020ea9590d05a5f8172b332da
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-11-30 01:12:53 +0700

    ีupdate เรียงเบอร์

--- bot.php
+++ bot.php
@@ -180,22 +180,6 @@ if (!is_null($events['events'])) {
 				$replyMsg .= " debug ".$debugmsg;
 			}
 
-			if(strstr($text,"check lotto")){
-
-				// Get RSS to JSON content
-				$lcontent = file_get_contents('https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Frssfeeds.sanook.com%2Frss%2Ffeeds%2Fsanook%2Fnews.lotto.xml');
-				// Parse JSON
-				$levents = json_decode($lcontent, true);
-				// Validate parsed JSON data
-
-				for ($i = 0; $i < 3; $i++) {
-
-				  $replyMsg .= "งวดวันที่: ".substr($levents['items'][$i]['pubDate'],0,10);
-				  $replyMsg .= chr(10);
-				  $replyMsg .= "ผลรางวัล:".chr(10);
-				  $replyMsg .= str_replace('<br>',chr(10),$levents['items'][$i]['content']);
-				}
-			}
 
 			// Build message to reply back
 
@@ -224,6 +208,23 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 
+		}
+
+		if(strstr($text,"เรียงเบอร์")){
+
+			// Get RSS to JSON content
+			$lcontent = file_get_contents('https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Frssfeeds.sanook.com%2Frss%2Ffeeds%2Fsanook%2Fnews.lotto.xml');
+			// Parse JSON
+			$levents = json_decode($lcontent, true);
+			// Validate parsed JSON data
+
+			for ($i = 0; $i < 3; $i++) {
+
+				$replyMsg .= "งวดวันที่: ".substr($levents['items'][$i]['pubDate'],0,10);
+				$replyMsg .= chr(10);
+				$replyMsg .= "ผลรางวัล:".chr(10);
+				$replyMsg .= str_replace('<br>',chr(10),$levents['items'][$i]['content']);
+			}
 		}
 
 	}elseif ($event['type'] == 'join') {
