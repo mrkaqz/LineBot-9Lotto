36b41579c63dfa63b4bfc375bccf5d876a309ba8
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-11-30 12:08:31 +0700

    update lotto.php

--- lotto.php
+++ lotto.php
@@ -6,14 +6,14 @@
     //งวด
     echo "งวดวันที่ ";
     $data = $dom->getElementById("spLottoDate");
-    echo $data->nodeValue."\n";
+    echo $data->nodeValue;
     echo "<br>";
 
     // รางวัลที่ 1
     echo "รางวัลที่ 1";
     echo "<br>";
     $data = $dom->getElementById("no1");
-    echo $data->nodeValue."\n";
+    echo $data->nodeValue;
     echo "<br>";
 
     //เลขหน้า 3 ตัว
@@ -21,7 +21,7 @@
     echo "<br>";
     for ($i = 1; $i <= 2; $i++) {
         $data = $dom->getElementById("d3:".$i);
-        echo $data->nodeValue."\n";
+        echo $data->nodeValue;
     }
     echo "<br>";
 
@@ -30,7 +30,7 @@
     echo "<br>";
     for ($i = 3; $i <= 4; $i++) {
         $data = $dom->getElementById("d3:".$i);
-        echo $data->nodeValue."\n";
+        echo $data->nodeValue;
     }
     echo "<br>";
 
@@ -38,7 +38,7 @@
     echo "เลขท้าย 2 ตัว";
     echo "<br>";
     $data = $dom->getElementById("d2");
-    echo $data->nodeValue."\n";
+    echo $data->nodeValue;
     echo "<br>";
 
     //ใกล้เคียงรางวัลที่ 1
@@ -46,7 +46,7 @@
     echo "<br>";
     for ($i = 1; $i <= 2; $i++) {
         $data = $dom->getElementById("no1nr:".$i);
-        echo $data->nodeValue."\n";
+        echo $data->nodeValue;
     }
     echo "<br>";
 
@@ -55,7 +55,7 @@
     echo "<br>";
     for ($i = 1; $i <= 5; $i++) {
         $data = $dom->getElementById("no2:".$i);
-        echo $data->nodeValue."\n";
+        echo $data->nodeValue;
     }
     echo "<br>";
 
@@ -64,7 +64,7 @@
     echo "<br>";
     for ($i = 1; $i <= 10; $i++) {
         $data = $dom->getElementById("no3:".$i);
-        echo $data->nodeValue."\n";
+        echo $data->nodeValue;
     }
     echo "<br>";
 
@@ -73,7 +73,7 @@
     echo "<br>";
     for ($i = 1; $i <= 50; $i++) {
         $data = $dom->getElementById("no4:".$i);
-        echo $data->nodeValue."\n";
+        echo $data->nodeValue;
     }
     echo "<br>";
 
@@ -82,6 +82,6 @@
     echo "<br>";
     for ($i = 1; $i <= 100; $i++) {
         $data = $dom->getElementById("no5:".$i);
-        echo $data->nodeValue."\n";
+        echo $data->nodeValue;
     }
 ?>
