2b34c1a35d68f58c48710c7ddc466c61fcefcba2
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-12-01 23:34:33 +0700

    new check

--- backup.php
+++ backup.php
@@ -199,17 +199,39 @@ if (!is_null($events['events'])) {
 
       }else{
 
-				$lottofinal = getlottoData($kurl);
+      // Get Lotto Date
+      $year = date('Y');
+      $month = date('m');
+      $day = date('d');
+      if ($day >= 16) {
+        $lottoday = "16";
+      }else{
+        $lottoday = "01";
+      }
+      $lottodate = $year."-".$month."-".$lottoday;
 
-				foreach ($lottofinal as $prize => $nlotto) {
-					if ($number == $nlotto){
+      $url = "http://www.glo.or.th/glo_seize/lottary/check_lottary.php";
 
-						$replyMsg .= "ถูกรางวัล";
-					}
+      $post_data = array (
+          "kuson" => 1,
+          "ldate" => "$lottodate",
+          "lnumber" => "$number",
+          "c_set" => ""
+      );
 
-				}
+      $ch = curl_init();
 
-/*
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      // we are doing a POST request
+      curl_setopt($ch, CURLOPT_POST, 1);
+      // adding the post variables to the request
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+      $output = curl_exec($ch);
+
+      curl_close($ch);
+
+
       if(strstr($output,"ไม่ถูกรางวัลสลากกินแบ่งรัฐบาล"))
       {
 
@@ -288,7 +310,7 @@ if (!is_null($events['events'])) {
           $replyMsg .= " ".$endword[rand(0,count($endword)-1)];
 					$replyMsg .= chr(10)."(งวดวันที่ ".$lottodate.")";
       }
-*/
+
       }
 
 			if(strstr($text,"debug ")){
