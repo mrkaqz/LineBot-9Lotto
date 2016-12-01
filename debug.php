f4ded98a96362128dbcac0d3b067f37e22660032
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-12-01 20:55:40 +0700

    debug

--- debug.php
+++ debug.php
@@ -6,7 +6,7 @@
     $begin = strpos($html, '//STARTPRIZE'); // Can hardcode this with 14 (the length of your 'needle'
     $end   = strpos($html, '//STOPPRIZE');
 
-    $text = substr($html, $begin, ($end - $begin));
+    $text = substr($html, $begin+24, ($end - $begin - 24));
 
     echo $text;
 /*
