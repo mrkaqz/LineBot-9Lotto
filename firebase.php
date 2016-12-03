55399a5aaadff2c3ddfcc30d96028ff348db2b24
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-12-03 20:04:04 +0700

    test

--- firebase.php
+++ firebase.php
@@ -7,13 +7,13 @@
 <form action="firebase.php" method="get">
 <select name="year">
   <option value="2016">2016</option>
-  <option value="2016">2015</option>
-  <option value="2016">2014</option>
-  <option value="2016">2013</option>
-  <option value="2016">2012</option>
-  <option value="2016">2011</option>
-  <option value="2016">2010</option>
-  <option value="2016">2009</option>
+  <option value="2015">2015</option>
+  <option value="2014">2014</option>
+  <option value="2013">2013</option>
+  <option value="2012">2012</option>
+  <option value="2011">2011</option>
+  <option value="2010">2010</option>
+  <option value="2009">2009</option>
 </select>
 <select name="month">
   <option value="01">01</option>
