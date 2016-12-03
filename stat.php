e57b34b66577bb8047b9fa7a353fcd8d693a2e9c
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-12-03 23:58:50 +0700

    test

---
+++ stat.php
@@ -0,0 +1,27 @@
+<?php
+
+$dburl = 'https://lotto-13fa4.firebaseio.com/result/.json';
+
+
+$ch = curl_init();
+
+// Read Firebase DB
+curl_setopt( $ch, CURLOPT_URL,$dburl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+
+$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+echo '<br> HTTP Code: ';
+echo $httpcode;
+echo '<br>';
+
+$data = json_encode($response);
+
+
+foreach ($data as $key => $value) {
+  echo $key.'<br />';
+}
+
+ ?>
