40ff2cb7a5282cd2903698fde811e201936ca529
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-12-04 18:41:44 +0700

    test

--- debug.php
+++ debug.php
@@ -8,6 +8,14 @@ foreach ($objstat[0] as $key => $value) {
     $d2array = $value;
 }
 
-print_r($d2array);
+echo $d2array[0];
+echo '<br />';
+echo $d2array[1];
+echo '<br />';
+echo $d2array[2];
+echo '<br />';
+echo $d2array[3];
+echo '<br />';
+
 
 ?>
