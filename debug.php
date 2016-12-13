ae895c4522d7b47e3fb43990d051e4dea798d9ee
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-12-14 01:31:45 +0700

    test

--- debug.php
+++ debug.php
@@ -21,18 +21,32 @@ curl_close($ch);
 echo $httpcode;
 */
 
-$client = new http\Client;
-$request = new http\Client\Request;
+// postman
 
-$request->setRequestUrl('https://api.line.me/v2/bot/group/Cbd5c975f4d1ad9bb2d782ee4d55539a1/leave');
-$request->setRequestMethod('POST');
-$request->setHeaders(array(
-  'postman-token' => '0f3db223-4832-52e8-8860-16ba594fb754',
-  'cache-control' => 'no-cache',
-  'authorization' => 'Bearer prIA1BgN1nWm2ieB6c9TkgBDSUQ7caE/VM/fHETGGtv1IyUqfJl79o0xwyW5GjtJC7DRrvix6SspnRw2R48NeFCkd/C0AxZt8Bt2yXJmDJRDHWl9gophkrplNu1LP2rwdONSJg0YszFlRwX+KRjMhAdB04t89/1O/w1cDnyilFU='
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.line.me/v2/bot/group/Cbd5c975f4d1ad9bb2d782ee4d55539a1/leave",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_HTTPHEADER => array(
+    "authorization: Bearer prIA1BgN1nWm2ieB6c9TkgBDSUQ7caE/VM/fHETGGtv1IyUqfJl79o0xwyW5GjtJC7DRrvix6SspnRw2R48NeFCkd/C0AxZt8Bt2yXJmDJRDHWl9gophkrplNu1LP2rwdONSJg0YszFlRwX+KRjMhAdB04t89/1O/w1cDnyilFU=",
+    "cache-control: no-cache",
+    "postman-token: 55cb89bb-4286-0c85-da76-f3f7748af0ac"
+  ),
 ));
 
-$client->enqueue($request)->send();
-$response = $client->getResponse();
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-echo $response->getBody();
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
