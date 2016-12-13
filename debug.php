d8782e92677914d37de50b2495a5c1bace5d0b25
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-12-14 01:29:52 +0700

    test

--- debug.php
+++ debug.php
@@ -21,20 +21,18 @@ curl_close($ch);
 echo $httpcode;
 */
 
-$request = new HttpRequest();
-$request->setUrl('https://api.line.me/v2/bot/group/Cbd5c975f4d1ad9bb2d782ee4d55539a1/leave');
-$request->setMethod(HTTP_METH_POST);
+$client = new http\Client;
+$request = new http\Client\Request;
 
+$request->setRequestUrl('https://api.line.me/v2/bot/group/Cbd5c975f4d1ad9bb2d782ee4d55539a1/leave');
+$request->setRequestMethod('POST');
 $request->setHeaders(array(
-  'postman-token' => 'c0360197-5b4f-dd35-d133-b28a9c224ce5',
+  'postman-token' => '0f3db223-4832-52e8-8860-16ba594fb754',
   'cache-control' => 'no-cache',
   'authorization' => 'Bearer prIA1BgN1nWm2ieB6c9TkgBDSUQ7caE/VM/fHETGGtv1IyUqfJl79o0xwyW5GjtJC7DRrvix6SspnRw2R48NeFCkd/C0AxZt8Bt2yXJmDJRDHWl9gophkrplNu1LP2rwdONSJg0YszFlRwX+KRjMhAdB04t89/1O/w1cDnyilFU='
 ));
 
-try {
-  $response = $request->send();
+$client->enqueue($request)->send();
+$response = $client->getResponse();
 
-  echo $response->getBody();
-} catch (HttpException $ex) {
-  echo $ex;
-}
+echo $response->getBody();
