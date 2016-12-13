afb6c3ac1eb3d1bb1f6ec750bdd5e3b6e8854ebb
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-12-14 01:16:45 +0700

    test

--- debug.php
+++ debug.php
@@ -1,29 +1,19 @@
 <?php
+$access_token = 'prIA1BgN1nWm2ieB6c9TkgBDSUQ7caE/VM/fHETGGtv1IyUqfJl79o0xwyW5GjtJC7DRrvix6SspnRw2R48NeFCkd/C0AxZt8Bt2yXJmDJRDHWl9gophkrplNu1LP2rwdONSJg0YszFlRwX+KRjMhAdB04t89/1O/w1cDnyilFU=';
+$groupid = 'Cbd5c975f4d1ad9bb2d782ee4d55539a1';
 
-$curl = curl_init();
+$leaveurl = 'https://api.line.me/v2/bot/group/'.$groupid.'/leave';
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.line.me/v2/bot/group/Cbd5c975f4d1ad9bb2d782ee4d55539a1/leave",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_HTTPHEADER => array(
-    "authorization: Bearer prIA1BgN1nWm2ieB6c9TkgBDSUQ7caE/VM/fHETGGtv1IyUqfJl79o0xwyW5GjtJC7DRrvix6SspnRw2R48NeFCkd/C0AxZt8Bt2yXJmDJRDHWl9gophkrplNu1LP2rwdONSJg0YszFlRwX+KRjMhAdB04t89/1O/w1cDnyilFU=",
-    "cache-control: no-cache",
-    "postman-token: e9d2f34f-6458-fc91-bc9c-5dd221305d22"
-  ),
-));
+$lheaders = array('Authorization: Bearer ' . $access_token);
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+$ch = curl_init($leaveurl);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $lheaders);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-curl_close($curl);
+curl_close($ch);
 
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
-}
+echo $httpcode;
