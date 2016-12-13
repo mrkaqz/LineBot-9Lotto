48970f3477b7bb2693aa882d7a32d4641135738a
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-12-14 01:12:07 +0700

    test

--- debug.php
+++ debug.php
@@ -1,19 +1,29 @@
-<?
+<?php
 
-$stathtml = file_get_contents('http://glacial-anchorage-14478.herokuapp.com/stat.php?type=json');
+$curl = curl_init();
 
-$objstat = json_decode($stathtml);
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.line.me/v2/bot/group/Cbd5c975f4d1ad9bb2d782ee4d55539a1/leave",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_HTTPHEADER => array(
+    "authorization: Bearer prIA1BgN1nWm2ieB6c9TkgBDSUQ7caE/VM/fHETGGtv1IyUqfJl79o0xwyW5GjtJC7DRrvix6SspnRw2R48NeFCkd/C0AxZt8Bt2yXJmDJRDHWl9gophkrplNu1LP2rwdONSJg0YszFlRwX+KRjMhAdB04t89/1O/w1cDnyilFU=",
+    "cache-control: no-cache",
+    "postman-token: e9d2f34f-6458-fc91-bc9c-5dd221305d22"
+  ),
+));
 
-foreach ($objstat[0] as $key => $value) {
-    $d2array = $value;
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
 }
-
-foreach ($d2array as $key => $value) {
-  echo 'Key: '.$key;
-  echo '<br />';
-  var_dump($value);
-  echo '<br />';
-}
-
-
-?>
