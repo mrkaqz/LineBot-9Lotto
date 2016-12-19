caad00bc06862e395fe5858c35f2d135a19885e6
Ronnarong Wongmalasit <Ronnarong Wongmalasit>
2016-12-19 19:46:42 +0700

    test

--- debug.php
+++ debug.php
@@ -1,53 +1,24 @@
 <?php
 
+// Get Lotto Date
+$year = 2017;
+$month = 1;
+$day = 10;
 
-$access_token = 'prIA1BgN1nWm2ieB6c9TkgBDSUQ7caE/VM/fHETGGtv1IyUqfJl79o0xwyW5GjtJC7DRrvix6SspnRw2R48NeFCkd/C0AxZt8Bt2yXJmDJRDHWl9gophkrplNu1LP2rwdONSJg0YszFlRwX+KRjMhAdB04t89/1O/w1cDnyilFU=';
-$groupid = 'Cbd5c975f4d1ad9bb2d782ee4d55539a1';
+if ( $month == 1 and $day < 16) {
+  $lottoday = "30";
+  $month = "12";
+  $year = $year-1;
+}else{
 
-$leaveurl = 'https://api.line.me/v2/bot/group/'.$groupid.'/leave';
-
-$lheaders = array('Authorization: Bearer prIA1BgN1nWm2ieB6c9TkgBDSUQ7caE/VM/fHETGGtv1IyUqfJl79o0xwyW5GjtJC7DRrvix6SspnRw2R48NeFCkd/C0AxZt8Bt2yXJmDJRDHWl9gophkrplNu1LP2rwdONSJg0YszFlRwX+KRjMhAdB04t89/1O/w1cDnyilFU=');
-
-$ch = curl_init('https://api.line.me/v2/bot/group/Cbd5c975f4d1ad9bb2d782ee4d55539a1/leave');
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $lheaders);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-curl_close($ch);
-
-echo $httpcode;
-
-
-// postman
-/*
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.line.me/v2/bot/group/Cbd5c975f4d1ad9bb2d782ee4d55539a1/leave",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_HTTPHEADER => array(
-    "authorization: Bearer prIA1BgN1nWm2ieB6c9TkgBDSUQ7caE/VM/fHETGGtv1IyUqfJl79o0xwyW5GjtJC7DRrvix6SspnRw2R48NeFCkd/C0AxZt8Bt2yXJmDJRDHWl9gophkrplNu1LP2rwdONSJg0YszFlRwX+KRjMhAdB04t89/1O/w1cDnyilFU=",
-    "cache-control: no-cache",
-    "postman-token: 55cb89bb-4286-0c85-da76-f3f7748af0ac"
-  ),
-));
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
+if ($day >= 16) {
+  $lottoday = "16";
+}else{
+  $lottoday = "01";
 }
-*/
+
+}
+
+$currentlottodate = $year.$month.$lottoday;
+
+echo $currentlottodate;
