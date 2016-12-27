fc35d9c4193a43302e5e2258d59d4a4f91dd628a
mrkaqz <mrkaqz@gmail.com>
2016-12-28 00:14:45 +0700

    test

---
+++ lottoset.php
@@ -0,0 +1,30 @@
+<?php
+
+$dburl = 'https://lotto-13fa4.firebaseio.com/result/.json';
+
+
+$ch = curl_init();
+
+// Read Firebase DB
+curl_setopt( $ch, CURLOPT_URL,$dburl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+
+$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+
+
+$data = json_decode($response);
+
+$lottoset = array();
+$i=0;
+foreach ($data as $key => $value) {
+  foreach ($value as $skey => $svalue) {
+    echo $skey.'->'.$svalue.'<br />';
+    if ($skey=='spLottoDate'){
+      $lottoset[$i]=$svalue;
+      $i++;
+    }
+  }
+}
