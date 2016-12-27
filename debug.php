8fbd66e247211a17bf56b5bf77a5d2b0800fb5f4
mrkaqz <mrkaqz@gmail.com>
2016-12-28 01:55:38 +0700

    test

--- debug.php
+++ debug.php
@@ -23,6 +23,12 @@ foreach ($data as $year => $value) {
     
     foreach ($svalue as $date => $check) {
       echo $date[0].$date[1].' : '.$check.'<br>';
+
+      if ($check == 'New') {
+        $nextlottodate = $year[0].$year[1].$year[2].$year[3].$month[0].$month[1].$date[0].$date[1];
+        echo 'Next Lotto Date : '.$nextlottodate;
+      }
+
     }
 
   }
