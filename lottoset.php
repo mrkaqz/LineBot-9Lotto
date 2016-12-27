d3f78838b29e352c96903383db3047a61508f477
mrkaqz <mrkaqz@gmail.com>
2016-12-28 00:19:22 +0700

    test

--- lottoset.php
+++ lottoset.php
@@ -20,6 +20,7 @@ $data = json_decode($response);
 $lottoset = array();
 $i=0;
 foreach ($data as $key => $value) {
+    echo $key.'->'.$value.'<br />'; 
   foreach ($value as $skey => $svalue) {
     //echo $skey.'->'.$svalue.'<br />';
     if ($skey=='spLottoDate'){
